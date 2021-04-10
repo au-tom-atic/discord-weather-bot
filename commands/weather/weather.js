a7376d505cdb7111a53db11b3fc1dddde759c5f9
Tom Leavy <trleavy@gmail.com>
2021-04-10 14:05:21 -0700

    added feels like, min/max, changed to embedded message'

--- commands/weather/weather.js
+++ commands/weather/weather.js
@@ -1,5 +1,6 @@
 const request = require("request");
 const dotenv = require("dotenv");
+const Discord = require("discord.js");
 dotenv.config();
 
 module.exports = {
@@ -18,17 +19,46 @@ module.exports = {
         request(url, function (err, response, body) {
             if (err) {
                 console.log("error:", error);
-                message.channel.send(error.body.message);
+                message.channel.send(
+                    "Sorry, we encountered an error. Please try again or Google the weather or something"
+                );
             } else {
-                console.log("body:", body);
                 let weather = JSON.parse(body);
                 try {
-                    const weatherResponse = `Current temp in ${weather.name} is ${weather.main.temp}\u00B0F`;
-                    message.channel.send(weatherResponse);
-                }
-                catch(error)
-                {
-                    message.channel.send('something went wrong, sorry');
+                    const weatherEmbeddedResponse = new Discord.MessageEmbed()
+                        .setColor("#0099ff")
+                        .setTitle(`Weather Report`)
+                        .setDescription(`Weather Report for ${weather.name}`)
+                        .setThumbnail(`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+                        .addFields(
+                            {
+                                name: "Description",
+                                value: `${weather.weather[0].main}, ${weather.weather[0].description}`,
+                            },
+                            {
+                                name: "Current temp",
+                                value: `${weather.main.temp}\u00B0`,
+                                inline: true,
+                            },
+                            {
+                                name: "Feels Like",
+                                value: `${weather.main.feels_like}\u00B0`,
+                                inline: true,
+                            },
+                            {
+                                name: "Min/Max",
+                                value: `${weather.main.temp_min}\u00B0/${weather.main.temp_max}\u00B0`,
+                                inline: true,
+                            }
+                        )
+                        .setTimestamp()
+                        .setFooter(
+                            "crazy weather we are having lately, right?",
+                        );
+                        message.channel.send(weatherEmbeddedResponse);      
+
+                } catch (error) {
+                    message.channel.send("something went wrong, sorry");
                 }
             }
         });
