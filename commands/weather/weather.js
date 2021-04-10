26aa477b3248ece1fd1248387a960f221f1e3014
Tom Leavy <trleavy@gmail.com>
2021-04-10 13:12:23 -0700

    get temperature for a city

--- commands/weather/weather.js
+++ commands/weather/weather.js
@@ -1,3 +1,7 @@
+const request = require("request");
+const dotenv = require("dotenv");
+dotenv.config();
+
 module.exports = {
     name: "weather",
     aliases: ["w"],
@@ -5,7 +9,28 @@ module.exports = {
     cooldown: 5,
     args: true,
     execute(message, args) {
-        const city = args.join(" ");
-        message.channel.send("one day I will get the weather for " + city);
+        const city = args.join("+");
+
+        let apiKey = process.env.WEATHER_KEY;
+        let cityName = city;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+
+        request(url, function (err, response, body) {
+            if (err) {
+                console.log("error:", error);
+                message.channel.send(error.body.message);
+            } else {
+                console.log("body:", body);
+                let weather = JSON.parse(body);
+                try {
+                    const weatherResponse = `Current temp in ${weather.name} is ${weather.main.temp}\u00B0F`;
+                    message.channel.send(weatherResponse);
+                }
+                catch(error)
+                {
+                    message.channel.send('something went wrong, sorry');
+                }
+            }
+        });
     },
 };
