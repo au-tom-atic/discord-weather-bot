6a2319ba86b2bd9aaa3a651618df03280a15e805
au-tom-atic <trleavy@gmail.com>
2022-01-28 15:40:07 -0800

    adding italics to command descriptions in help msg

--- commands/util/help.js
+++ commands/util/help.js
@@ -23,31 +23,31 @@ module.exports = {
         .addFields(
             {
                 name: '`weather w minimal m [LOCATION]`',
-                value: `Provides a concise weather report for [LOCATION]`,
+                value: `*Provides a concise weather report for [LOCATION]*`,
             },
             {
                 name: '`forecast f [LOCATION]`',
-                value: `Provides a 3 day forecast for [LOCATION]`,
+                value: `*Provides a 3 day forecast for [LOCATION]*`,
             },
             {
                 name: '`detailed d dw [LOCATION]`',
-                value: `Provides a detailed weather report for [LOCATION]`,
+                value: `*Provides a detailed weather report for [LOCATION]*`,
             },
             {
                 name: '`alert advisory a [LOCATION]`',
-                value: `Provides weather alerts for [LOCATION]`,
+                value: `*Provides weather alerts for [LOCATION]*`,
             },
             {
                 name: '`units u [imperial | metric]`',
-                value: `Save preferences for metric or imperial`,
+                value: `*Save preferences for metric or imperial*`,
             },
             {
                 name: '`location l [LOCATION]`',
-                value: `Remember this [LOCATION] for weather, detailed weather, and alert commands.`,
+                value: `*Remember this [LOCATION] for weather, detailed weather, and alert commands*`,
             },
             {
                 name: '`help h`',
-                value: `betterhelp.com/zeroweather for 10% off your first month`,
+                value: `*betterhelp.com/zeroweather for 10% off your first month*`,
             }
         )
         .setTimestamp()
