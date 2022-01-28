b323aeea6abc8843872a254bf609211dc00173ae
au-tom-atic <trleavy@gmail.com>
2022-01-28 15:20:23 -0800

    making commands in help message display as inline code

--- commands/util/help.js
+++ commands/util/help.js
@@ -10,7 +10,7 @@ module.exports = {
         .setTitle(`Help`)
         .setAuthor(
             `Help Message`,
-            "https://static.thenounproject.com/png/967229-200.png",
+            "https://github.com/au-tom-atic/discord-weather-bot",
             "https://github.com/au-tom-atic/discord-weather-bot"
         )
         .setURL("https://openweathermap.org")
@@ -19,31 +19,31 @@ module.exports = {
         )
         .addFields(
             {
-                name: `weather w minimal m [LOCATION]`,
+                name: '`weather w minimal m [LOCATION]`',
                 value: `Provides a concise weather report for [LOCATION]`,
             },
             {
-                name: `forecast f [LOCATION]`,
+                name: '`forecast f [LOCATION]`',
                 value: `Provides a 3 day forecast for [LOCATION]`,
             },
             {
-                name: `detailed d dw [LOCATION]`,
+                name: '`detailed d dw [LOCATION]`',
                 value: `Provides a detailed weather report for [LOCATION]`,
             },
             {
-                name: ` alert advisory a [LOCATION]`,
+                name: '`alert advisory a [LOCATION]`',
                 value: `Provides weather alerts for [LOCATION]`,
             },
             {
-                name: `units u [imperial | metric]`,
+                name: '`units u [imperial | metric]`',
                 value: `Save preferences for metric or imperial`,
             },
             {
-                name: `location l [LOCATION]`,
+                name: '`location l [LOCATION]`',
                 value: `Remember this [LOCATION] for weather, detailed weather, and alert commands.`,
             },
             {
-                name: `help h`,
+                name: '`help h`',
                 value: `betterhelp.com/zeroweather for 10% off your first month`,
             }
         )
