6354b90b2f3109475bf0f57256f76ee39d41b455
au-tom-atic <trleavy@gmail.com>
2022-02-06 17:30:00 -0800

    adding converting commands

--- commands/util/ctof.js
+++ commands/util/ctof.js
@@ -2,12 +2,16 @@ const axios = require("axios");
 const Discord = require("discord.js");
 
 module.exports = {
-    name: "convert",
-    aliases: ["c", "conversion"],
+    name: "CtoF",
+    aliases: ["c2f", "fahrenheit"],
     description: "Converts a temperature from C to F, or F to C",
     cooldown: 15,
     args: true,
     async execute(message, args) {
-        message.reply(`This feature is coming soon!`);
+        const celsisus = args[0]
+
+        const fahrenheit = (celsisus * 1.8) + 32
+
+        message.channel.send(`${fahrenheit}°F`);
     },
 };
---
+++ commands/util/ftoc.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "FtoC",
+    aliases: ["f2c", "celsius", "c"],
+    description: "Converts a temperature from C to F, or F to C",
+    cooldown: 15,
+    args: true,
+    async execute(message, args) {
+        const fahrenheit = args[0]
+
+        const celsisus = (fahrenheit - 32) * 0.555
+
+        message.channel.send(`${celsisus}°C`);
+    },
+};
